2cbb3f24e9a27bc34d768a1c1e37ff27fc07ca44
Denis Bobrov <delneg@yandex.ru>
2021-03-23 01:28:12 +0300

    Fix runtests.py

--- runtests.py
+++ runtests.py
@@ -14,7 +14,7 @@ def run_tests(*test_args):
     if not test_args:
         test_args = ['tests']
 
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'example.settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
