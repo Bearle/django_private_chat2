9be04d67e18f514d3ad4084c981900b119192b90
Denis Bobrov <delneg@yandex.ru>
2021-03-27 15:24:18 +0300

    Working fetch users, use Context.Data

--- example/LoadTest/Program.fs
+++ example/LoadTest/Program.fs
@@ -19,39 +19,6 @@ type UserInfoResponse =
                       username = get.Required.Field "username" Decode.string
                     }
                 )
-type DialogModel =
-      {
-       id: int
-       created: DateTimeOffset
-       modified: DateTimeOffset
-       other_user_id: string
-       unread_count: int
-       username: string
-      }
-      static member Decoder: Decoder<DialogModel> =
-          Decode.object (fun get ->
-              {
-                  id = get.Required.Field "id" Decode.int
-                  created = (get.Required.Field "created" Decode.int64) |> DateTimeOffset.FromUnixTimeSeconds
-                  modified = (get.Required.Field "modified" Decode.int64) |> DateTimeOffset.FromUnixTimeSeconds
-                  other_user_id = get.Required.Field "other_user_id" Decode.string
-                  unread_count = get.Required.Field "unread_count" Decode.int
-                  username = get.Required.Field "username" Decode.string
-              })
-
-    type DialogsResponse =
-        { page: int
-          pages: int
-          data: DialogModel array
-          }
-        static member Decoder : Decoder<DialogsResponse> =
-            Decode.object
-                (fun get ->
-                    { page = get.Required.Field "page" Decode.int
-                      pages = get.Required.Field "pages" Decode.int
-                      data = get.Required.Field "data" (Decode.array DialogModel.Decoder)
-                    }
-                )
 
 [<EntryPoint>]
 let main argv =
@@ -61,7 +28,7 @@ let main argv =
     let backendUrl = "http://127.0.0.1:8000"
     let loginEndpoint = sprintf "%s/dumb_auth/" backendUrl
     let messagesEndpoint = sprintf "%s/messages/" backendUrl
-    let dialogsEndpoint = sprintf "%s/dialogs/" backendUrl
+    let usersEndpoint = sprintf "%s/users/" backendUrl
     let selfEndpoint = sprintf "%s/self/" backendUrl
 
     let getCookieValue (s:string) =
@@ -85,42 +52,48 @@ let main argv =
                 let sessionId = cookie |> Option.bind (Array.tryItem 1)
 
                 match csrfToken, sessionId with
-                | Some token, Some i -> return Response.Ok ($"csrftoken={token}; sessionid={i}")
+                | Some token, Some i ->
+                    context.Data.["cookie"] <- $"csrftoken={token}; sessionid={i}"
+                    return Response.Ok ()
                 | _ -> return Response.Fail("status code: " + response.StatusCode.ToString())
             })
     )
 
 
     let fetch_self_step = HttpStep.create("fetch_self", fun context ->
-        let cookie = context.GetPreviousStepResponse<string>()
-
         Http.createRequest "GET" selfEndpoint
-        |> Http.withHeader "Cookie" cookie
+        |> Http.withHeader "Cookie" (string context.Data.["cookie"])
         |> Http.withCheck(fun response -> task {
                 let! json = response.Content.ReadAsStringAsync()
                 let decoded = Decode.fromString UserInfoResponse.Decoder json
 
                 match decoded with
-                | Result.Ok _ -> return Response.Ok(cookie)
+                | Result.Ok d ->
+                    context.Data.["selfInfo"] <- $"{d.pk},{d.username}"
+                    return Response.Ok()
                 | Result.Error _ -> return Response.Fail()
             })
     )
 
-    let fetch_dialogs_step = HttpStep.create("fetch_dialogs", fun context ->
-        Http.createRequest "GET" dialogsEndpoint
+    let fetch_users_step = HttpStep.create("fetch_dialogs", fun context ->
+        Http.createRequest "GET" usersEndpoint
         |> Http.withHeader "Accept" "application/json"
+        |> Http.withHeader "Cookie" (string context.Data.["cookie"])
         |> Http.withCheck(fun response -> task {
                 let! json = response.Content.ReadAsStringAsync()
-                let decoded = Decode.fromString DialogsResponse.Decoder json
+                let decoded = Decode.fromString (Decode.array UserInfoResponse.Decoder) json
                 match decoded with
-                | Result.Ok dialogs -> return Response.Ok(dialogs.data |> Array.map (fun x -> x.other_user_id))
+                | Result.Ok dialogs ->
+                    let dialogIds = dialogs|> Array.map (fun x -> x.pk) |> String.concat ","
+                    context.Data.["dialogIds"] <- dialogIds
+                    return Response.Ok()
                 | Result.Error _ -> return Response.Fail()
             })
     )
 
 
 
-    Scenario.create "django_private_chat2_loadtest" [login_step;fetch_self_step]
+    Scenario.create "django_private_chat2_loadtest" [login_step;fetch_self_step;fetch_users_step]
 //    |> Scenario.withWarmUpDuration(seconds 1)
     |> Scenario.withLoadSimulations [KeepConstant(1, seconds 1)]
     |> NBomberRunner.registerScenario
